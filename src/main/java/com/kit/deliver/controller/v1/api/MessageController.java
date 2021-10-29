4294fba253cfb3ce17c6cc553d959ef89909c40c
jihainan <alwaysbegoing@163.com>
2021-10-29 15:36:13 +0800

    feat: add TaskController and MessageController TaskController.java: operations about managing messages and rules MessageController.java: operations about getting messages

---
+++ src/main/java/com/kit/deliver/controller/v1/api/MessageController.java
@@ -0,0 +1,18 @@
+package com.kit.deliver.controller.v1.api;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MessageController
+ * @Description Operations pertaining to get messages based on user characteristics in deliver application
+ * @Author jihainan
+ * @Date 2021/10/29 10:40 上午
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("api/v1/message")
+@Api(value = "deliver-application")
+public class MessageController {
+}
---
+++ src/main/java/com/kit/deliver/controller/v1/api/TaskController.java
@@ -0,0 +1,18 @@
+package com.kit.deliver.controller.v1.api;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MessageController
+ * @Description Operations pertaining to publish task in deliver application
+ * @Author jihainan
+ * @Date 2021/10/29 1:10 上午
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("api/v1/deliver/")
+@Api(value = "deliver-application")
+public class TaskController {
+}
