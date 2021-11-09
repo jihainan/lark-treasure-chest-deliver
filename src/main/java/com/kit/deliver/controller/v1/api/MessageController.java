d2f59c5e85a8dff81bc711639b8741d3d0acb8e3
jihainan <alwaysbegoing@163.com>
2021-11-09 10:50:22 +0800

    feat: add api & query message GetMessageRequest.java: request model MessageController.java: request api

--- src/main/java/com/kit/deliver/controller/v1/api/MessageController.java
+++ src/main/java/com/kit/deliver/controller/v1/api/MessageController.java
@@ -1,8 +1,20 @@
 package com.kit.deliver.controller.v1.api;
 
+import com.kit.deliver.controller.v1.request.GetMessageRequest;
+import com.kit.deliver.dto.model.MessageDto;
+import com.kit.deliver.dto.response.Response;
+import com.kit.deliver.service.MessageService;
+import com.kit.deliver.service.RuleService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * @ClassName MessageController
@@ -15,4 +27,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/message")
 @Api(value = "deliver-application")
 public class MessageController {
+
+    private final MessageService messageService;
+    private final RuleService ruleService;
+
+    @Autowired
+    public MessageController(MessageService messageService, RuleService ruleService) {
+        this.messageService = messageService;
+        this.ruleService = ruleService;
+    }
+
+    @PostMapping("/query")
+    @ApiOperation(value = "query messages using user characteristic")
+    public Response<List<MessageDto>> queryMessages(@RequestBody @Valid GetMessageRequest getMessageRequest) {
+        return Response
+                .ok();
+    }
 }
